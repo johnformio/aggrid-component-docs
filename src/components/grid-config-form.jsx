c7af909c47718b5c2d43c331b43b1bcc3c5a3e0a
John Teague <164385719+johnformio@users.noreply.github.com>
2025-06-11 10:38:23 -0500

    A grid configuration example using updateGridConfig (#1)
    Co-authored-by: John Teague <johnt@formio.com>

--- src/components/grid-config-form.jsx
+++ src/components/grid-config-form.jsx
@@ -4,38 +4,45 @@ import {spaceMissionForm} from '../data/space-mission-form.js';
 spaceMissionForm.components[0].agGridConfig.rowData = rowData;
 
 const onFormReadyHandler = (webform) => {
-    setTimeout(() => {
+    // This function is called when the form is ready
+    console.log('Form is ready:', webform);
       const agGridComponent = webform.getComponent("aggrid");
-      const agGrid = agGridComponent?.agGrid;
-      agGrid.updateGridOptions({
-        columnDefs:[
-            { field: "company", headerName: "Company", pinned: true },
-            { field: "mission", headerName: "Mission" },
-            
-            { field: "location", headerName: "Location" },
-            { field: "date", headerName: "Date" },
-            { field: "time", headerName: "Time" },
-            { field: "rocket", headerName: "Rocket" },
-            { field: "price", headerName: "Price" },
-            { field: "successful", headerName: "Successful" },
-        ]
+      agGridComponent.on('agGridReady', ({api}) => {
+        api.updateGridOptions({
+            columnDefs:[
+                { field: "company", headerName: "Company", pinned: true },
+                { field: "mission", headerName: "Mission" },
+                
+                { field: "location", headerName: "Location" },
+                { field: "date", headerName: "Date" },
+                { field: "time", headerName: "Time" },
+                { field: "rocket", headerName: "Rocket" },
+                { field: "price", headerName: "Price" },
+                { field: "successful", headerName: "Successful" },
+            ],
+            selectionColumnDef: {
+                sortable: true,
+                resizable: true,
+                // width: 120,
+                suppressHeaderMenuButton: false,
+                pinned: "left",
+              },
+        })
       })
       
-    }, 100);
-  };
+      
+      
+};
+
 
 export default function GridConfigForm() {
-    
-    
     return (
-        
         <Form
             form={spaceMissionForm}
             onFormReady={onFormReadyHandler}
             onSubmit={(submission) => {
                 console.log('Submission:', submission);
               }}
-        />
-        
+        />  
     );
-    }
+}
