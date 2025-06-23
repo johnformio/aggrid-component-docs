efc69ceb23fd359844915a07717e8b65e2b0d900
John Teague <johnt@formio.com>
2025-06-23 17:54:03 -0500

    added formio styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import { useMemo, useState } from 'react'
 
 import './App.css'
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import bootstrap5 from '@formio/bootstrap/bootstrap5';
-
-
 
 
 import {FormioProvider } from "@formio/react";
@@ -17,7 +14,6 @@ import Layout from './Layout';
 import Home from './Home.jsx';
 import { MDXProvider } from "@mdx-js/react";
 import MdxLayout from './mdx-layout';
-Formio.use(bootstrap5);
 Formio.license = // Live license with premium components
   "eyJhbGciOiJQUzI1NiJ9.eyJsaWNlbnNlTmFtZSI6IkxpdmUgTGljZW5zZSBQcmVtaXVtIFZpa2EiLCJpYXQiOjE3NDg3MjUyMDAsImV4cCI6MTc4MDMwNDQwMCwiZmFpbERhdGUiOjE3ODE2MDA0MDAsInRlcm1zIjp7InByZW1pdW0iOnRydWUsIm9mZmxpbmUiOmZhbHNlLCJlbnRlcnByaXNlQnVpbGRlciI6ZmFsc2UsInJlcG9ydGluZyI6ZmFsc2UsInNxbGNvbm5lY3RvciI6ZmFsc2UsImVzaWduIjpmYWxzZSwiZW5kcG9pbnRzIjpbIiouZm9ybS5pbyJdLCJob3N0bmFtZXMiOlsiKi5jc2IuYXBwIl19LCJpc3MiOiJodHRwczovL2Zvcm0uaW8iLCJzdWIiOiJGb3JtLmlvIn0.rH2GGEan6M6uZxnoT8rZxjb6PmkxPu2D8Ucf5eM8CcOUqVjfM-XjT-sZ5o5LXBx-uQPbcG3MifD5TMXaUdkA9uTEDBxuvehYnMUiDr981OKffNKI6lCrrbZA5q-dRB8Pd9pl8AvfSE27MKgJLrmtlNp0-8HSlmQO_wCZNPxwu85dT6TNLzgO6e1kBInAeLUMwH78kutOhWaMCSlEhBu3vgxV2owVItYO_fjoUn6e_Xxy_FhFegPeFlTMl8ypX2nlRFpu1Ypogh8P6-OkKTHSq4ThILeU9LoXFlhwnYc4yT6MYgolzlvVDH-WN9joiQLDMf2sVS5a4Yvwi6P2jR6I_A";
 Formio.use(premium);
