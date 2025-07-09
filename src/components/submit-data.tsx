4a257066759a660ad7472bfc3afe498059b6c302
Jacob Nichols <jacobnichols@form.io>
2025-07-09 12:10:23 -0500

    creating submission updates select submission list

--- src/components/submit-data.tsx
+++ src/components/submit-data.tsx
@@ -1,7 +1,6 @@
 'use client';
-import { Form, Submission } from '@formio/react';
-import { useEffect, useState } from 'react';
-import { useFormioContext } from '@formio/react';
+import {Form, Submission, useFormioContext} from '@formio/react';
+import {useEffect, useState} from 'react';
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from '@/components/ui/select';
 
 const form = 'http://localhost:3000/ureruosrbpknfgf/agtest';
@@ -9,16 +8,19 @@ const form = 'http://localhost:3000/ureruosrbpknfgf/agtest';
 type SubmissionsSelectionProps = {
     submissions: Submission[];
     onSelectChange: (value: string) => void;
+    value: string
 };
 
-const SubmissionsSelection = ({ submissions, onSelectChange }: SubmissionsSelectionProps) => {
-
+const SubmissionsSelection = ({submissions, onSelectChange, value}: SubmissionsSelectionProps) => {
     return (
-        <Select onValueChange={onSelectChange}>
+        <Select onValueChange={onSelectChange} value={value}>
             <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Select Submission" />
+                <SelectValue placeholder="Select Submission"/>
             </SelectTrigger>
             <SelectContent>
+                <SelectItem key={"0"} value={"No Selection"}>
+                    {"Remove Current Selection"}
+                </SelectItem>
                 {submissions.map((submission, i) => (
                     <SelectItem key={submission._id as string} value={i.toString()}>
                         {i.toString()}
@@ -30,9 +32,8 @@ const SubmissionsSelection = ({ submissions, onSelectChange }: SubmissionsSelect
 }
 
 
-
 export default function SubmitData() {
-    const { Formio } = useFormioContext();
+    const {Formio} = useFormioContext();
     const [submissions, setSubmissions] = useState<Submission[]>([]);
     const [submissionsLoading, setSubLoading] = useState(true);
     const [selectedSubmissionIndex, setSelectedSubmissionIndex] = useState<number | null>(null);
@@ -42,7 +43,6 @@ export default function SubmitData() {
             try {
                 const formio = new Formio(form);
                 const data: Submission[] = await formio.loadSubmissions();
-                data.forEach((submission) => delete submission._id);
                 setSubmissions(data);
             } catch (error) {
                 console.error('Error loading submissions:', error);
@@ -50,29 +50,42 @@ export default function SubmitData() {
                 setSubLoading(false);
             }
         };
-        
+
         fetchSubmissions();
     }, []);
-    
+
     function handleSelectChange(value: string): void {
+        if (value === 'No Selection') {
+            setSelectedSubmissionIndex(null);
+            return;
+        }
         console.log(`Selected submission ID: ${value}`);
 
         setSelectedSubmissionIndex(Number(value));
     }
 
-    
+
     if (submissionsLoading) return <div>Loading submissions...</div>;
-    
-    
+
+
     function handleFormSubmit(submission: Submission): void {
         console.log('Form submitted:', submission);
+        setSelectedSubmissionIndex(null);
+        if (submissions.some((value) => submission._id === value._id)) {
+            return;
+        }
+        const tmpSubmissions = [...submissions];
+        tmpSubmissions.push(submission);
+        setSubmissions(tmpSubmissions);
     }
 
     return (
         <div>
             <h1>Select Submission</h1>
-            <SubmissionsSelection submissions={submissions} onSelectChange={handleSelectChange}  />
-            <Form src={form} onSubmit={handleFormSubmit} submission={selectedSubmissionIndex !== null ? structuredClone(submissions[selectedSubmissionIndex]) : undefined} />
+            <SubmissionsSelection submissions={submissions} onSelectChange={handleSelectChange}
+                                  value={selectedSubmissionIndex !== null ? String(selectedSubmissionIndex) : ''}/>
+            <Form src={form} onSubmit={handleFormSubmit}
+                  submission={selectedSubmissionIndex !== null ? structuredClone(submissions[selectedSubmissionIndex]) : undefined}/>
         </div>
     );
 }
